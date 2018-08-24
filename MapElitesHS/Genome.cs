c8236c4d83b4a348fd1f53b280a926adfe67e5b1
scotchkorean27 <scottlee27@hotmail.com>
2018-08-23 23:57:23 -0700

    Update Genome.cs

--- MapElitesHS/Genome.cs
+++ MapElitesHS/Genome.cs
@@ -2,11 +2,20 @@
 {
     public class Genome
     {
-        public double fitnessPerformance = 0;
+        private double fitnessPerformance = 0;
 
+        // Maybe move this out of this class in case we want to do coevolution?
         public double calculateFitnessPerformance()
         {
             return 0;
+        }
+        public double getFitness()
+        {
+            return fitnessPerformance;
+        }
+        public ArrayList getDeck() 
+        {
+            return null;
         }
         public BehaviorVector convertGenomeToBehaviorVector()
         {
