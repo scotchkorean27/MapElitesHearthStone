26cb495d160a08c8e8d9dde58170381877492aac
scotchkorean27 <scottlee27@hotmail.com>
2018-08-23 23:55:21 -0700

    Update Genome.cs

--- MapElitesHS/Genome.cs
+++ MapElitesHS/Genome.cs
@@ -2,11 +2,20 @@
 {
     public class Genome
     {
-        public double fitnessPerformance = 0;
+        private double fitnessPerformance = 0;
 
+        // Maybe move this out of this class and turn Genome into an immutable data structure?
         public double calculateFitnessPerformance()
         {
             return 0;
+        }
+        public double getFitness()
+        {
+            return fitnessPerformance;
+        }
+        public ArrayList getDeck() 
+        {
+            return null;
         }
         public BehaviorVector convertGenomeToBehaviorVector()
         {
